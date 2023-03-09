8c535cadb12e45972c0cca880cb7079198104c48
praisejah-moses <praisejah.moses22@gmail.com>
2023-03-09 17:27:47 +0100

    fix

--- flicks-server/server.js
+++ flicks-server/server.js
@@ -2,7 +2,7 @@
 import  express  from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import  dotenv from './dotenv'
+import  dotenv from 'dotenv'
 import path from 'path'
 import bodyParser from 'body-parser';
 
